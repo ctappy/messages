b2ae554950e8567930cbbcdae1ba8bdf2f6aab7d
Colby Taperts <colbytaperts@gmail.com>
2019-02-23 10:02:31 -0600

    add in go get for protoc setup

--- generate.sh
+++ generate.sh
@@ -2,6 +2,8 @@
 
 echo running...
 
+if ! which protoc-gen-go 1> /dev/null; then go get -u github.com/golang/protobuf/protoc-gen-go; fi
+
 set -ex
 # Setup greet pb file
 protoc messagepb/message.proto --go_out=plugins=grpc:.
